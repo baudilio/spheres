d2a4650382a81d4350d9d71394ea6a823e4b362f
Baudilio Tejerina (@Augusta) <bltejerina@gmail.com>
2020-10-21 19:56:01 -0500

    add staffKujilaars method

--- lib/types.f90
+++ lib/types.f90
@@ -16,3 +16,43 @@ MODULE spheretypes
   END TYPE dnode
 
 END MODULE spheretypes
+
+Module algo
+  USE spheretypes, ONLY: wp => SP, nodes => snode
+  implicit none
+
+  ! The method of Saff and Kuijlaars arranges the nodes along a spiral
+  ! in such a way that the distance between nodes along the spiral is
+  ! approximately equal to the distance between coils of the spiral:
+contains
+
+  subroutine staffKujilaars(N, node)
+    implicit none
+    ! - dummy parameters
+    integer, intent(in) :: N
+    type(nodes), Dimension(N), intent(out) :: node
+
+    ! - Local Variables
+    real(wp) :: s
+    real(wp) :: dz
+    real(wp) :: long = 0.0_wp
+    real(wp) :: z
+    real(wp) :: r
+    integer :: i
+
+    s = 3.6/sqrt(Real(N, wp))
+    dz = 2.0/N
+    z = 1 - dz/2
+
+    do i = 1, N
+      r = sqrt(1.0_wp - z*z)
+      node(i)%x = r * cos(long)
+      node(i)%y = r * sin(long)
+      node(i)%z = z
+      z = z - dz
+      long = long + s/r
+    end do
+
+  end subroutine staffKujilaars
+
+end Module algo
--- prog/main.f90
+++ prog/main.f90
@@ -1,5 +1,6 @@
 PROGRAM main
   USE spheretypes, ONLY : PI => sPi, node => snode
+  USE algo, ONLY : staffKujilaars
   IMPLICIT NONE
 
   ! -- Local variables
@@ -7,8 +8,18 @@ PROGRAM main
   INTEGER, PARAMETER :: N = 128
   LOGICAL :: aleatory = .TRUE.
   TYPE(node), Dimension(N) :: nodes
+  INTEGER :: ilun
 
+  ! - staffKujilaars method
+  CALL staffKujilaars(N, nodes)
+  !print '(I5/"Staffkujilaars")', N
+  !print "('H ',3F9.5)", node
+  OPEN(Newunit=ilun, FILE="staffKujilaars.xyz", Action="Write")
+  WRITE(ilun, FMT='(I5/"staffKujilaars")') n
+  WRITE(ilun, FMT=100) nodes
+  CLOSE(ilun)
 
+100	FORMAT('H ', 3F9.5)
   print *, "All Done - Have a Great Day."
   STOP 0
 END PROGRAM main
